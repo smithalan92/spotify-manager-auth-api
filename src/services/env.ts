99c1a748c1cdc47f55872dba9c8b2fa66c652f28
Alan Smith <smithalan2@gmail.com>
2022-06-30 01:46:17 +0100

    Explicitly add env values

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,9 +1,11 @@
 import { Env } from "../lib/types";
 
-export default function makeEnv() {
+export default function makeEnv(): Env {
   return {
-    ...process.env,
-    SERVER_PORT: 3420,
-    serviceName: "Spotify Manager Auth API",
-  } as unknown as Env;
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID!,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET!,
+    SPOTIFY_REDIRECT_URL: process.env.SPOTIFY_REDIRECT_URL!,
+    SERVER_PORT: process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 3420,
+    serviceName: "Spotify Auth Proxy API",
+  };
 }
